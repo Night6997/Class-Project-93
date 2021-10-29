d7c2352fa26900a6c2522c36184a499beb5ebdf8
Night6997 <83875915+Night6997@users.noreply.github.com>
2021-10-29 17:44:42 +0530

    Add files via upload

--- kwitter_page.js
+++ kwitter_page.js
@@ -28,11 +28,44 @@ function send(){
 
 }
 
-function getData() { firebase.database().ref("/"+room_name).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
+function getData() { firebase.database().ref("/"+Roomname).on('value', function(snapshot) { document.getElementById("output").innerHTML = ""; snapshot.forEach(function(childSnapshot) { childKey  = childSnapshot.key; childData = childSnapshot.val(); if(childKey != "purpose") {
          firebase_message_id = childKey;
          message_data = childData;
-//Start code
+         console.log(firebase_message_id);
+         console.log(message_data);
+         msg=message_data['msg'];
+         name=message_data['name'];
+         like=message_data['like'];
+         name_with_tag="<h4>" +name+ "<img class='tick_mark' src='tick.png'> </h4>";
+         message_with_tag="<h4 class='h4_message'>"+msg+ "</h4>";
+         like_with_tag="<button id="+firebase_message_id+" class='btn btn-warning' value="+like+" onclick='updateLike(this.id)'>";
+         span_with_tag="<span class='glyphicon glyphicon-thumbs-up'> Like, "+like+"</span> </button> <hr>";
 
-//End code
+         row=name_with_tag+message_with_tag+like_with_tag+span_with_tag;
+         document.getElementById("output").innerHTML +=row;
       } });  }); }
 getData();
+
+function updateLike(message_id){
+
+      console.log("clicked on like button"+message_id);
+      button_id=message_id;
+      updatedLikes= documen.getElementById(button_id).value;
+      IncreasedLikes = Number(updatedLikes)+1;
+      console.log(IncreasedLikes);
+      firebase.database().ref(Roomname).child(message_id).update({
+
+            like:IncreasedLikes
+      
+      });
+
+}
+
+function logout(){
+
+      localStorage.removeItem("username");
+      localStorage.removeItem("inputRoom");
+      window.location="index.html";
+
+}
+
